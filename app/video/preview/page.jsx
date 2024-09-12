182243ed9ce149ed7ac62a9f96dae61e5a729822
duckatgit <duckatgit@gmail.com>
2024-09-12 19:01:43 +0530

    done chnages and added new video preview page

--- app/video/preview/page.jsx
+++ app/video/preview/page.jsx
@@ -74,7 +74,7 @@ const preview_video = () => {
             <div className='text-[#E5AD00]'> upload new Thumbnail  </div>
           </div>
           <div className='mt-4 px-4 border-2 rounded-xl'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever sinc.when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </div>
           <div className='flex flex-wrap justify-between bg-gray-300  border-r '>
             <div className='flex gap-4 m-4'>
@@ -121,10 +121,10 @@ const preview_video = () => {
             </div>
           </div>
         </div>
-        <div className='w-1/2 m-4'>
+        <div className='w-1/2 '>
           <Label htmlFor="email">Email Body (Must Include “Video” Tag)</Label>
-          <div className='mt-2 px-4 border-2 rounded-xl'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          <div className='mt-2 px-2 border-2 rounded-xl m-2 p-2'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </div>
           <div className='flex flex-wrap justify-between bg-gray-300  border-r '>
             <div className='flex gap-4 m-4'>
