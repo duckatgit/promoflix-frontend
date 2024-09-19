28e2e25f765041f0ebf764b59adcae258a803fba
duckatgit <duckatgit@gmail.com>
2024-09-19 18:53:32 +0530

    added instance get video and get script api

--- app/home/dashboard/page.jsx
+++ app/home/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import {
   flexRender,
   getCoreRowModel,
@@ -17,148 +17,23 @@ import {
   Upload,
 } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
 import { NameLogo } from "@/components/ui/name-logo";
 import { useToast } from "@/hooks/use-toast";
 import { useRouter } from 'next/navigation';
 import { Input } from "@/components/ui/input";
 import axios from "axios";
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import Header from "@/app/auth/header/page";
+import Dashboard from "@/components/dashboard";
 
 // Mock Data
 const token = localStorage.getItem("token");
 const DataTableDemo = () => {
   const { toast } = useToast();
   const router = useRouter();
-  const [sorting, setSorting] = useState([]);
   const [allInstances, setAllInstances] = useState([]);
   const [instance, setInstance] = useState();
-  const [instanceId, setInstanceId] = useState("");
-
-  const [columnFilters, setColumnFilters] = useState([]);
-  const [columnVisibility, setColumnVisibility] = useState({});
-  const [rowSelection, setRowSelection] = useState({});
-  const [fileModal, setFileModal] = useState(false);
-  const [updateInstanceModal, setupdateInstanceModal] = useState(false);
-
-  const fileInputRef = useRef(null);
-  const handleUploadClick = () => {
-    setFileModal(true);
-  };
-  let openUpdateInstanceModal = (name, id) => {
-    setInstance(name);
-    setInstanceId(id)
-    setupdateInstanceModal(true);
-  }
-
-  const columns = [
-    {
-      id: "serial",
-      header: "S.No",
-      cell: ({ row }) => row.index + 1, // Displaying row index + 1
-      enableSorting: false,
-      enableHiding: false,
-    },
-    {
-      accessorKey: "name",
-      header: ({ column }) => (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Video Title
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
-      ),
-      cell: ({ row }) => <div className="lowercase">{row.getValue("name")}</div>,
-    },
-    {
-      accessorKey: "action",
-      header: () => <div className="text-right">Action</div>,
-      cell: ({ row }) => {
-        let instance_id = row.original.id
-        let instance_name = row.getValue("name"); // Assuming "name" is the key for the instance name
-
-        const deleteInstance = async () => {
-          try {
-            const response = await axios.delete('http://54.225.255.162/api/v1/instance', {
-              headers: {
-                'Authorization': `Bearer ${token}`,
-              },
-              params: {
-                id: instance_id
-              },
-            });
-            if (response.data.code != 200) {
-              toast({
-                variant: "destructive",
-                title: "Uh oh! Something went wrong.",
-                description: data.result,
-              })
-            }
-            else {
-              toast({
-                description: "Instance deleted sucessfully",
-              })
-              getAllInstance()
-            }
-          } catch (error) {
-            console.log(error)
-          }
-        };
-
-        return (
-          <div className="flex justify-end space-x-2">
-            <Edit className="h-4 w-4 text-grey" onClick={() => openUpdateInstanceModal(instance_name, instance_id)} ></Edit>
-            <AlertDialog>
-              <AlertDialogTrigger>
-                <Trash className="h-4 w-4 text-red-500" />
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Delete</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    Do you really want to delete this item?
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                  <AlertDialogAction className="bg-[#FF2E00]" onClick={deleteInstance}>Continue</AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </div>
-        );
-      },
-    },
-  ];
-  let getAllInstance = async () => {
+  const getAllInstance = async () => {
     try {
       const response = await axios.get('http://54.225.255.162/api/v1/instance', {
         headers: {
@@ -189,7 +64,6 @@ const DataTableDemo = () => {
       getAllInstance()
     }, []
   )
-  console.log(allInstances, 'allInstances')
   const createInstance = async () => {
     try {
       if (instance) {
@@ -231,177 +105,15 @@ const DataTableDemo = () => {
       });
     }
   };
-  const updateInstance = async () => {
-    try {
-      const response = await fetch('http://54.225.255.162/api/v1/instance', {
-        method: 'PUT',
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id: instanceId,
-          name: instance,
-          "data_id": null,
-          "video_id": null
-        }),
-      });
-      const data = await response.json();
-      if (data.code != 200) {
-        toast({
-          variant: "destructive",
-          title: "Uh oh! Something went wrong.",
-        });
-      } else {
-        toast({
-          description: "Instence Updated successfully",
-        });
-        setInstance("")
-
-      }
-    } catch (error) {
-      console.log(error, '===========error')
-    }
-  }
-
-  const table = useReactTable({
-    data: allInstances,
-    columns,
-    onSortingChange: setSorting,
-    onColumnFiltersChange: setColumnFilters,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    onColumnVisibilityChange: setColumnVisibility,
-    onRowSelectionChange: setRowSelection,
-    state: {
-      sorting,
-      columnFilters,
-      columnVisibility,
-      rowSelection,
-    },
-  });
 
   return (
     <div className="mx-8">
+
       <Header />
-      <div className="w-full">
-        <Button
-          className="flex justify-end ml-auto text-black"
-          style={{ backgroundColor: "#FFC000" }}
-          onClick={handleUploadClick}
-        >
-          <Upload className="h-4 w-4" />
-          Create Instance
-        </Button>
+      <Suspense fallback={<p>Loading feed..........</p>}>
+        <Dashboard instance={instance} setInstance={setInstance} createInstance={createInstance} allInstances={allInstances} getAllInstance={getAllInstance} />
+      </Suspense>
 
-        <Dialog open={fileModal} onOpenChange={setFileModal}>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Create Instance
-              </DialogTitle>
-            </DialogHeader>
-            <div className="gap-4 border-dashed justify-center flex flex-col m-4 items-center">
-              <div className="flex flex-col space-y-1.5 w-[400px]">
-                <input className="h-10 rounded-md" type="text" value={instance} onChange={(e) => { setInstance(e.target.value) }} />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button variant="outline" onClick={() => setFileModal(false)}>
-                Cancel
-              </Button>
-              <Button
-                className="bg-[#FFC000] text-black"
-                onClick={() => {
-                  createInstance();
-                  setFileModal(false);
-                  getAllInstance()
-                }}
-              >
-                Submit
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-        <Dialog open={updateInstanceModal} onOpenChange={setupdateInstanceModal}>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Update Instance
-              </DialogTitle>
-            </DialogHeader>
-            <div className="gap-4 border-dashed justify-center flex flex-col m-4 items-center">
-              <div className="flex flex-col space-y-1.5 w-[400px]">
-                <input className="h-10 rounded-md" type="text" value={instance} onChange={(e) => { setInstance(e.target.value) }} />
-              </div>
-            </div>
-            <DialogFooter>
-              <Button variant="outline" onClick={() => setupdateInstanceModal(false)}>
-                Cancel
-              </Button>
-              <Button
-                className="bg-[#FFC000] text-black"
-                onClick={() => {
-                  updateInstance();
-                  setupdateInstanceModal(false);
-                  getAllInstance()
-                }}
-              >
-                Update
-              </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
-
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map(headerGroup => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map(header => (
-                  <TableHead key={header.id}>
-                    {flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                ))}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows.map(row => (
-              <TableRow key={row.id}>
-                {row.getVisibleCells().map(cell => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
-        </div>
-        <div className="space-x-2">
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            Previous
-          </Button>
-          <Button
-            variant="outline"
-            size="sm"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            Next
-          </Button>
-        </div>
-      </div>
     </div>
   );
 };
--- app/video/preview/page.jsx
+++ app/video/preview/page.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import Header from "../../auth/header/page";
 import { Button } from '@/components/ui/button';
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useRouter } from 'next/navigation';
 import {
   Select,
   SelectContent,
@@ -16,148 +19,199 @@ import {
   Trash,
   Upload,
 } from "lucide-react";
+import Image from 'next/image';
 const preview_video = () => {
-  const arr = ['first name', "last name", "email", "address", "company"]
-  const videoURL = 'https://rr5---sn-5hne6nzs.googlevideo.com/videoplayback?expire=1726162148&ei=hNDiZq6EBuKM6dsPnsepkQM&ip=195.123.218.149&id=o-AGi-O5LV1GjZapXJAS2Wzr2hasqiSu4AhXDQtA0BxqFT&itag=18&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=rw&mm=31%2C29&mn=sn-5hne6nzs%2Csn-5hneknes&ms=au%2Crdu&mv=m&mvi=5&pl=23&initcwndbps=282500&bui=AQmm2exaM6IAwGK_gi1E3MVNZxgNo2tJIFTNd96PelvhoJYDGocK6Op8Xbwg8ujz5pgG5xBQuKu5-lKn&spc=Mv1m9rZoANGLTRP-3gXP7EGnsOLTwPv_QfOdRSpe17Pr09dUib2a9jw&vprv=1&svpuc=1&mime=video%2Fmp4&ns=P7YsEKm7ZVuoKUJzw8tAwSgQ&rqh=1&cnr=14&ratebypass=yes&dur=159.126&lmt=1709261862564317&mt=1726140088&fvip=4&c=WEB_CREATOR&sefc=1&txp=4538434&n=h-If6va70bYMYw&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Ccnr%2Cratebypass%2Cdur%2Clmt&sig=AJfQdSswRQIhAIsdx7IxQin016_TWaOBMQ_Ob0dkK58uIbI7dD1_MUCHAiBP3MK3pShJ57Nb9urI8fS6vPAEZ8QVkXH8b67iiH0AFg%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=ABPmVW0wRAIgZWSEBmSnC4bVspeXp0vgbm2R-ljMadYifOzPcSGcpc4CIBx5wWNYMi0zjB0LX7zuCqk2RwdBwFcYhd_Ms108EI5P&title=New%20BUDOT%20dance%202021%20%5BMonkey%5D'; // Replace with your video URL
+  const token = localStorage.getItem("token");
+  const [videoUrl, setVideoUrl] = useState();
+  const [videoThumb, setVideoThumb] = useState("");
+  const [transcript, setTranscript] = useState("");
 
+
+
+
+  const myfunction = async (id) => {
+    try {
+      const response = await fetch(`http://54.225.255.162/api/v1/file/${id}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      const get_transcript = await fetch(`http://54.225.255.162/api/v1/transcript/${id}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      const video_data = await response.json();
+      const transcript_data = await get_transcript.json();
+      console.log(video_data, "data")
+      if (video_data.code == 200) {
+        setVideoUrl(video_data.result.url)
+        setVideoThumb(video_data.result.thumbnail)
+      }
+      if (transcript_data.code == 200) {
+        setTranscript(transcript_data.result.transcript)
+      }
+    } catch (error) {
+      console.log(error, '==========error')
+    }
+  }
+  useEffect(
+    () => {
+      let id = window.location.href.split("id=")[1]
+      if (id) {
+
+        myfunction(id)
+      }
+    }, []
+  )
+  const video_url = videoUrl
+  console.log(video_url, 'video_url')
+  const arr = ['first name', "last name", "email", "address", "company"]
   return (
     <div className='m-8'>
       <Header />
-      <div className='flex justify-between'>
-        <div className='w-1/2 m-4'>
-          <video width="600" controls>
-            <source src={videoURL} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          <p className='mt-2'>Video Title</p>
-        </div>
-        <div className='w-1/2 m-4 shadow-2xl'>
-          <div className='flex flex-wrap justify-between bg-gray-300  border-r'>
-            <div className='flex gap-4 m-4'>
-              {arr?.map(
-                (i) => {
-                  return <p className='bg-white p-2 rounded-xl'> {i} </p>
-                }
-              )}
+      <div>
+        <div className='flex justify-between'>
+          <div className='w-1/2 m-4'>
+            {videoUrl &&
+              <video width="600" controls>
+                <source src={videoUrl} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>}
+            <p className='mt-2'>Video Title</p>
+          </div>
+          <div className='w-1/2 m-4 shadow-2xl'>
+            <div className='flex flex-wrap justify-between bg-gray-300  border-r'>
+              <div className='flex gap-4 m-4'>
+                {arr?.map(
+                  (i) => {
+                    return <p className='bg-white p-2 rounded-xl'> {i} </p>
+                  }
+                )}
+              </div>
             </div>
-          </div>
-          <div className='mt-4 px-4'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </div>
-        </div>
-      </div>
-      <h1 className='m-4'>Upload Data From Document</h1>
-      <div className='m-4 border-2 rounded-md	'>
-        <div className='m-4 flex b'>
-          <div className='my-4 '>
-            <Button className="bg-[#28434B]"> Upload Data</Button>
-          </div>
-          <div className='my-6 mx-4'>
-            <p>or </p>
-          </div>
-          <div className="flex-col space-y-1.5 my-4">
-            <Input id="email" type="text" placeholder="Paste Spreadsheet URL" />
-          </div>
-        </div>
-        <div className='m-4 text-gray-500'>
-          File Must be .xls, .xlsx, .xlsm, .xlt, .xltx, . (Excel or google a sheet).
-        </div>
-      </div>
-      <h1 className='mx-4 mt-2 mb-2'>Video Thumbnail</h1>
-      <div className='flex justify-between border-2 rounded-lg'>
-        <div className='w-1/2 m-4'>
-          <img className='rounded-2xl' src="https://i.iheart.com/v3/re/new_assets/610952da451455560ad79f24?ops=max(1060,0),quality(80)" alt="" />
-        </div>
-        <div className='w-1/2 m-4'>
-          <div className='h-16 w-64 bg-[#FFF4D3] gap-2 rounded-xl p-2 justify-center items-center flex flex-col'>
-            <div className='text-[#E5AD00]'>  < Upload />  </div>
-            <div className='text-[#E5AD00]'> upload new Thumbnail  </div>
-          </div>
-          <div className='mt-4 px-4 border-2 rounded-xl'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever sinc.when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </div>
-          <div className='flex flex-wrap justify-between bg-gray-300  border-r '>
-            <div className='flex gap-4 m-4'>
-              {arr?.map(
-                (i) => {
-                  return <p className='bg-white p-2 rounded-xl'> {i} </p>
-                }
-              )}
-            </div>
-          </div>
-          <div className='my-2'>
-            <Button className="bg-[#FFC000] text-black">Upload
-            </Button>
-            <Button className="bg-[#FFC000] text-black ml-3">Reset all
-            </Button>
-          </div>
-        </div>
-      </div>
-      <h1 className='mx-4 mt-2 mb-2'>Video Thumbnail</h1>
-      <div className='flex justify-between border-2 rounded-lg'>
-        <div className='w-1/2 m-4'>
-          <div>
-            <Label htmlFor="email">Email Videos from</Label>
-            <Select>
-              <SelectTrigger className="w-[580px] my-2">
-                <SelectValue placeholder="Platform" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div className='my-20'>
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" value="Hey, {{First name}}, I made a video for you" />
-            <div className='flex my-4'>
-              {arr?.map(
-                (i) => {
-                  return <p className='bg-white p-2 rounded-3xl border-2'> {i} </p>
-                }
-              )}
+            <div className='mt-4 px-4'>
+              {transcript}
             </div>
           </div>
         </div>
-        <div className='w-1/2 '>
-          <Label htmlFor="email">Email Body (Must Include “Video” Tag)</Label>
-          <div className='mt-2 px-2 border-2 rounded-xl m-2 p-2'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </div>
-          <div className='flex flex-wrap justify-between bg-gray-300  border-r '>
-            <div className='flex gap-4 m-4'>
-              {arr?.map(
-                (i) => {
-                  return <p className='bg-white p-2 rounded-xl'> {i} </p>
-                }
-              )}
+        <h1 className='m-4'>Upload Data From Document</h1>
+        <div className='m-4 border-2 rounded-md	'>
+          <div className='m-4 flex b'>
+            <div className='my-4 '>
+              <Button className="bg-[#28434B]"> Upload Data</Button>
+            </div>
+            <div className='my-6 mx-4'>
+              <p>or </p>
+            </div>
+            <div className="flex-col space-y-1.5 my-4">
+              <Input id="email" type="text" placeholder="Paste Spreadsheet URL" />
             </div>
           </div>
-          <div className='my-2 mb-2'>
-            <Button className="bg-[#FFC000] text-black">Send to All 145 Emails
-            </Button>
-            <Button className="bg-[#FFC000] text-black ml-3">Reset all
-            </Button>
+          <div className='m-4 text-gray-500'>
+            File Must be .xls, .xlsx, .xlsm, .xlt, .xltx, . (Excel or google a sheet).
           </div>
-          <Label htmlFor="email">First just send a test email to: </Label>
-          <div className='flex'>
-            <Select>
-              <SelectTrigger className="w-[580px] my-2">
-                <SelectValue placeholder="Platform" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-            <Button className="bg-[#FFC000] text-black ml-1 mt-2">Send
-            </Button>
+        </div>
+        <h1 className='mx-4 mt-2 mb-2'>Video Thumbnail</h1>
+        <div className='flex justify-between border-2 rounded-lg'>
+          <div className='w-1/2 m-4 relative h-[500px]'>
+            <Image layout='fill' className='rounded-2xl   object-cover' src={videoThumb} alt="" />
+          </div>
+          <div className='w-1/2 m-4'>
+            <div className='h-16 w-64 bg-[#FFF4D3] gap-2 rounded-xl p-2 justify-center items-center flex flex-col'>
+              <div className='text-[#E5AD00]'>  < Upload />  </div>
+              <div className='text-[#E5AD00]'> upload new Thumbnail  </div>
+            </div>
+            <div className='mt-4 px-4 border-2 rounded-xl'>
+              {transcript}
+            </div>
+            <div className='flex flex-wrap justify-between bg-gray-300  border-r '>
+              <div className='flex gap-4 m-4'>
+                {arr?.map(
+                  (i) => {
+                    return <p className='bg-white p-2 rounded-xl'> {i} </p>
+                  }
+                )}
+              </div>
+            </div>
+            <div className='my-2'>
+              <Button className="bg-[#FFC000] text-black">Upload
+              </Button>
+              <Button className="bg-[#FFC000] text-black ml-3">Reset all
+              </Button>
+            </div>
+          </div>
+        </div>
+        <h1 className='mx-4 mt-2 mb-2'>Video Thumbnail</h1>
+        <div className='flex justify-between border-2 rounded-lg'>
+          <div className='w-1/2 m-4'>
+            <div>
+              <Label htmlFor="email">Email Videos from</Label>
+              <Select>
+                <SelectTrigger className="w-[580px] my-2">
+                  <SelectValue placeholder="Platform" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className='my-20'>
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" value="Hey, {{First name}}, I made a video for you" />
+              <div className='flex my-4'>
+                {arr?.map(
+                  (i) => {
+                    return <p className='bg-white p-2 rounded-3xl border-2'> {i} </p>
+                  }
+                )}
+              </div>
+            </div>
+          </div>
+          <div className='w-1/2 '>
+            <Label htmlFor="email">Email Body (Must Include “Video” Tag)</Label>
+            <div className='mt-2 px-2 border-2 rounded-xl m-2 p-2'>
+              {transcript}
+            </div>
+            <div className='flex flex-wrap justify-between bg-gray-300  border-r '>
+              <div className='flex gap-4 m-4'>
+                {arr?.map(
+                  (i) => {
+                    return <p className='bg-white p-2 rounded-xl'> {i} </p>
+                  }
+                )}
+              </div>
+            </div>
+            <div className='my-2 mb-2'>
+              <Button className="bg-[#FFC000] text-black">Send to All 145 Emails
+              </Button>
+              <Button className="bg-[#FFC000] text-black ml-3">Reset all
+              </Button>
+            </div>
+            <Label htmlFor="email">First just send a test email to: </Label>
+            <div className='flex'>
+              <Select>
+                <SelectTrigger className="w-[580px] my-2">
+                  <SelectValue placeholder="Platform" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+              <Button className="bg-[#FFC000] text-black ml-1 mt-2">Send
+              </Button>
+            </div>
           </div>
         </div>
       </div>
+
+
     </div >
   )
 }
