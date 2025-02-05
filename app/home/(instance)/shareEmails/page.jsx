e4a88171592078bebc57f41a8e5d6e64f975a18d
sanjeev <sanjeev@barkleyuw.com>
2025-02-05 16:05:16 +0530

    implemented send  sample email api and UI fixes

--- app/home/(instance)/shareEmails/page.jsx
+++ app/home/(instance)/shareEmails/page.jsx
@@ -12,12 +12,16 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import StickyBanner from "../user-emails/Banner";
 import { useSearchParams } from "next/navigation";
+import { postData } from "@/utils/api";
+import { LoadingSpinner } from "@/components/ui/spinner";
+import { useToast } from "@/hooks/use-toast";
 
 const ShareEmails = () => {
   const router = useRouter();
+  const { toast } = useToast();
   const searchParams = useSearchParams();
   const [emailData, setEmailData] = useAtom(emailsAtom);
-  const [selectedEmail, setSelectedEmail] = useState("");
+  const [selectedEmail, setSelectedEmail] = useState({email:"",emai_id:''});
   const [column, setColumn] = useState("");
   const [subject, setSubject] = useState("");
   const [body, setBody] = useState("");
@@ -27,22 +31,21 @@ const ShareEmails = () => {
 
   const array = arrayParam ? JSON.parse(arrayParam) : [];
   const userArray = userArrayParam ? JSON.parse(userArrayParam) : [];
+  const [errors, setErrors] = useState({email:false,body:false,subject:false});
+  const [loading, setLoading] = useState(false);
 
   const handleClick = (item) => {
     // Append the clicked item in the format {{item}} to the subject
-    setSubject((prevSubject) => `${prevSubject} {{${item}}}`);
+    setSubject((prevSubject) => `${prevSubject} {${item}}`);
+    setErrors((prev) => ({ ...prev, subject:false}));
+
   };
   const handleClickBody = (item) => {
     // Append the clicked item in the format {{item}} to the subject
-    setBody((prevSubject) => `${prevSubject} {{${item}}}`);
+    setBody((prevSubject) => `${prevSubject} {${item}}`);
+    setErrors((prev) => ({ ...prev, body:false }));
   };
-  const handleChange = (e) => {
-    setSubject(e.target.value);
-  };
-  const handleChangeBody = (e) => {
-    setBody(e.target.value);
-  };
-
+ 
   const filterArray = (array, excludeKeywords) => {
     // Ensure the array exists and is an array
     if (!Array.isArray(array)) return [];
@@ -54,6 +57,67 @@ const ShareEmails = () => {
     );
   };
 
+  const validateForm = () => {
+    let newErrors = { ...errors };
+
+    if (!selectedEmail?.email) {
+      newErrors.email = true;
+    }
+
+    if (!subject) {
+      newErrors.subject = true;
+    }
+
+    if (!body) {
+      newErrors.body = true;
+    }
+    setErrors(newErrors); 
+
+    if (!newErrors.email && !newErrors.subject && !newErrors.body) {
+      return true;
+    }
+
+    return false; 
+  };
+  const sendSampleEmail = async () => {
+      try {
+        if(validateForm()){
+        setLoading(true);
+        const payload={
+          email_id:selectedEmail?.id,
+          subject:subject,
+          body:body,
+
+        }
+        const response = await postData(`api/share/sample/${id}`,payload,"share_dev");
+
+        if (response.code != 200) {
+          setLoading(false);
+          toast({
+            variant: "destructive",
+            title: "Uh oh! Something went wrong.",
+            description: data.result,
+          })
+        }
+        else {
+          setLoading(false);
+          toast({
+            type: "success",
+            description: "sample email send successfully.",
+          })
+        }
+      }
+      } catch (error) {
+        setLoading(false);
+        console.log('error.message', error.message)
+        toast({
+          type: "error",
+          title: "Uh oh! Something went wrong.",
+          description: error?.message,
+        })
+  
+      }
+    }
   return (
     <>
       {emailData?.length > 0 ? (
@@ -86,16 +150,22 @@ const ShareEmails = () => {
                   </Link>
                 </label>
                 <select
-                  className="w-full p-2 mb-[20px] border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
-                  value={selectedEmail}
-                  onChange={(e) => setSelectedEmail(e.target.value)}
+                  className={`w-full p-2 ${!errors?.email?'mb-[20px] border border-gray-300 ':"border border-red-500"} rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500`}
+                  value={selectedEmail?.email}
+                  onChange={(e) => {
+                    const selectedOption = emailData.find((ele) => ele.email === e.target.value);
+                    setSelectedEmail({ email: e.target.value, id: selectedOption?.id });
+                    setErrors((prev) => ({ ...prev, email: e.target.value ? false : true }));
+                  }}
                 >
+                  <option value="">Select an email</option>
                   {emailData?.map((ele) => (
-                    <option key={ele?.email} value={ele?.email}>
+                    <option id={ele?.id} value={ele?.email}>
                       {ele?.email}
                     </option>
                   ))}
                 </select>
+                {errors.email && <p className="text-red-500 mb-5">Please select an email</p>}
               </div>
               {/* Select Column */}
               <div>
@@ -123,7 +193,7 @@ const ShareEmails = () => {
                   onChange={(e) => setColumn(e.target.value)}
                 >
                   <option value="">Select a column</option>
-                  {filterArray(array, ["url", "video", "thumbnail", "gif", "status"])?.map((ele) => (
+                  {filterArray(array, ["url", "Video", "thumbnail", "gif", "status"])?.map((ele) => (
                     <option key={ele} value={ele}>
                       {ele}
                     </option>
@@ -145,15 +215,17 @@ const ShareEmails = () => {
                   ))}
                 </div>
                 <input
+                  id="subject"
                   type="text"
                   placeholder="Hey {{First Name}}..."
-                  className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  className={`w-full p-2 ${!errors?.subject?'border border-gray-300 ':"border border-red-500"} rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500`}
                   value={subject}
-                  onChange={handleChange}
+                  onChange={(e) => {
+                    setSubject(e.target.value);
+                    setErrors((prev) => ({ ...prev, subject: e.target.value ? false : true }));
+                  }}
                 />
-                {/* <p className="text-red-500 text-xs mt-1">
-                  The email subject should not be empty
-                </p> */}
+                {errors.subject && <p className="text-red-500">The email subject should not be empty</p>}
               </div>
               {/* Buttons */}
               <div className="flex flex-col gap-2 mt-5">
@@ -165,9 +237,15 @@ const ShareEmails = () => {
                 </Button>
                 <Button
                   className="w-full text-blue-500 bg-white hover:text-white hover:bg-blue-500 font-medium border border-blue-500  rounded-2xl"
-                  onClick={""}
+                  onClick={() => sendSampleEmail()}
                 >
-                  Send me a sample email
+                  {loading ? (
+                    <>
+                      Send me a sample email <LoadingSpinner className="ml-2 text-black" />
+                    </>
+                  ) : (
+                    "Send me a sample email"
+                  )}
                 </Button>
               </div>
             </div>
@@ -176,27 +254,32 @@ const ShareEmails = () => {
               {/* Body */}
               <div>
                 <h1 className="font-semibold leading-6 mb-[2px]">
-                  {"Body (must include {{video}}) variable) all csv headers + video, gif, thumbnail"}
+                  {"Body (must include {{video}}) variable)"}
                 </h1>
                 <div className="my-3 flex gap-2 flex-wrap">
-                  {filterArray(array, "status")?.map((item, i) => (
+                  {filterArray(array, ["status", "thumbnail", "gif"])?.map((item, i) => (
                     <div key={i}
                       className="py-2 px-6 text-blue-500 bg-white   font-medium border border-blue-500  rounded-2xl cursor-pointer"
-                      onClick={() => handleClickBody(item)}
-                    >{item}</div>
+                      onClick={() => handleClickBody(item==='Video'?'Video*':item)}
+                    >{item==='Video'?'Video*':item}</div>
                   ))}
                 </div>
                 <textarea
+                  id='body'
                   placeholder="Hey {{First Name}}..."
                   rows="7"
-                  className="w-full p-2 border border-gray-300  rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+                  className={`w-full p-2 ${!errors?.body?'border border-gray-300 ':"border border-red-500"} rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500`}
                   value={body}
-
-                  onChange={handleChangeBody}
+                  onChange={(e) => {
+                    setBody(e.target.value);
+                    setErrors((prev) => ({ ...prev, body: e.target.value ? false : true }));
+                  }}
                 ></textarea>
+                {errors.body && <p className="text-red-500">The body should not be empty</p>}
+                {errors.body && <p className="text-red-500">The body should contain the <code>{"{{Video*}}"}</code> and <code>{"{variable 1}"}</code></p>}
               </div>
             </div>
-          </div>
+          </div>     
         </div>
       ) : (
         <div className="w-full ">
